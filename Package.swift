43441bc218b2001812dfd08cd4184a830d0b079d
Ali Najjar <ali.najjar@harri.com>
2026-02-26 06:08:38 +0200

    [iOS] Package dependencies upgrade (ENG-22181)
     - [iOS] Upgrade JNSegmentedControl Library (ENG-21458)
      - [iOS] Upgrade JNSegmentedControl Library (ENG-22524)
       - resources changes

--- Package.swift
+++ Package.swift
@@ -18,9 +18,7 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "JNSegmentedControl",
-            resources: [
-                .process("Resources"),
-            ]),
+            name: "JNSegmentedControl"
+        ),
     ]
 )
